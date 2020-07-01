a5742aa4e537ce882071d6111bda421a519c7861
Artem Romanov <romanov-artyom@bk.ru>
2020-07-01 20:15:10 +0300

    Conditions without loops

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,53 @@
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+//        String str = "some string";
+//        String rusStr = "какая-то строка";
+//        String partStr = "string";
+//
+//        if (str.contains(rusStr)) {
+//            System.out.println("Строка str содержит в себе строку rus_str");
+//        } else if (str.contains(partStr)) {
+//            System.out.println("Строка str содержит в себе строку part_str");
+//        } else {
+//            System.out.println("Строка str не содержит ни одну из строк");
+//        }
+
+//        int firstNum = 1;
+//        int secondNum = 2;
+//        int thirdNum = 3;
+//
+//        if (firstNum < secondNum) {
+//            System.out.println("Первое число меньше второго");
+//        } else if (secondNum < thirdNum) {
+//            System.out.println("Второе число меньше третьего");
+//        } else if (firstNum != thirdNum) {
+//            System.out.println("Первое и третье числа не равны");
+//        } else {
+//            System.out.println("Слишком сложно!");
+//        }
+//
+//        if (secondNum < thirdNum) {
+//            System.out.println("Второе число меньше третьего");
+//        } else {
+//            System.out.println("Слишком сложно!");
+//        }
+
+
+        // Если нет break, то выполнится весь остальной код после подходящего случая (case)
+//        int num = 4;
+//        switch (num) {
+//            case 1:
+//                System.out.println("число равно 1");
+//                break;
+//            case 2:
+//                System.out.println("число равно 2");
+//                break;
+//            case 3:
+//                System.out.println("число равно 3");
+//                break;
+//            default:
+//                System.out.println("число не равно 1, 2, 3");
+//        }
     }
 }
