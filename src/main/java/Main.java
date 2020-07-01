512837588d65bf588b6cdd6843fa77a865555420
Artem Romanov <romanov-artyom@bk.ru>
2020-07-01 18:32:42 +0300

    Lesson's materials

---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+//        byte b = 127;
+//        short sh = 32767;
+//        int i = 9223;
+//        long l = 9223L;
+//
+//        float fl = 100.10f;
+//        double d = 100.10d;
+//
+//        int a = 10;
+//        float res = (float)a - (float)9.8;
+//        System.out.format("%.2f",res);
+
+//        boolean tr = true;
+//        boolean fa = false;
+//
+//        System.out.println(3+7);
+//        System.out.println(3>7);
+//        System.out.println(3<7);
+//        System.out.println(5==5);
+//        System.out.println(5!=5);
+//        System.out.println("abc".equals("abc"));
+//        System.out.println("abc".equals("cba"));
+
+//        char a = 'p';
+//        System.out.println((short)a);
+
+//        String str = "some string";
+//        String rus_str = "какая-то строка";
+//        System.out.println(str.contains(rus_str));
+//        System.out.println(str.equals(rus_str));
+//        System.out.println(str.concat(rus_str));
+//        System.out.println(str.indexOf('m'));
+
+
+//        int[] array_int = {1, 2, 3};
+//        int[] array_int2 = new int[3];
+//        System.out.println(array_int[2]);
+//
+//        String[] array_str = {"one", "two", "three"};
+//        System.out.println(array_str[2]);
+//        System.out.println(array_int.length);
+//        array_int[0] = 10;
+//        array_int[1] = 10;
+//        array_int[2] = 10;
+//        for (int i : array_int) {
+//            System.out.println(i);
+//        }
+//        System.out.println();
+//        for (int i : array_int2) {
+//            System.out.println(i);
+//        }
+
+//        ArrayList<Integer> list = new ArrayList();
+//        list.add(0);
+//        list.add(1);
+//        list.add(3);
+//        System.out.println(list.size());
+//        for (int i : list){
+//            System.out.println(i);
+//        }
+//        System.out.println(list.get(2));
+//
+//        list.add(10);
+//        System.out.println(list.size());
+//        System.out.println("Добавлено значение 10");
+//        for (int i : list){
+//            System.out.println(i);
+//        }
+    }
+}
