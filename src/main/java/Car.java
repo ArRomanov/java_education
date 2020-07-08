43a92efbfdea8ee6a5e5de848069855d6c0b49f3
Artem Romanov <romanov-artyom@bk.ru>
2020-07-08 19:16:29 +0300

    Car objects

---
+++ src/main/java/Car.java
@@ -0,0 +1,49 @@
+public class Car {
+    public Car(double engVolume) {
+        this.engVolume = engVolume;
+        maxSpeed = engVolume * 100;
+        System.out.println("Создана машина с объемом двигателя " + engVolume);
+    }
+
+    private double engVolume;
+    private String transmission;
+    private int year;
+    private String color;
+    private double speed = 0;
+    public double maxSpeed;
+
+    public void speedUp() throws InterruptedException {
+        System.out.println("Ускоряемся!");
+        while (true) {
+            System.out.println(speed);
+            speed = speed + engVolume * 10;
+            Thread.sleep(500);
+            if (speed >= maxSpeed) {
+                break;
+            }
+        }
+
+    }
+
+    public void getBreak() throws InterruptedException {
+        System.out.println("Тормозим!");
+        while (true) {
+            System.out.println(speed);
+            speed = speed - engVolume * 20;
+            Thread.sleep(500);
+            if (speed <= 0) {
+                speed = 0;
+                break;
+            }
+        }
+    }
+
+    public void turnOn(String where) {
+        System.out.println("Машина повернула " + where);
+    }
+
+    public double getCurrentSpeed() {
+        return speed;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Car firstCar = new Car(1.6);
+        Car secondCar = new Car(3.2);
+
+        firstCar.speedUp();
+        firstCar.getBreak();
+
+        secondCar.speedUp();
+        secondCar.getBreak();
+
+        if (firstCar.maxSpeed > secondCar.maxSpeed) {
+            System.out.println("Скорость первой машины больше скорости второй машины");
+        }
     }
 }
